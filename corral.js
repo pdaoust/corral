f251980cfc899a276954883f3135ada06892ba7a
Paul d'Aoust <paul@heliosstudio.ca>
2012-09-25 17:28:12 -0700

    refactored heavily; see full log entry for changes. * now uses more consistent action and fence names for event * corrals themselves now handle media query checking (via this.check()) * if an event sees that its complement matches it, it'll fire the ___-both event * other stuff that I can't remember

--- corral.js
+++ corral.js
@@ -1,17 +1,21 @@
 (function (win) {
-	var resizeEventTimeoutID;
-
 	var exists = function exists (val) {
 		return val !== undefined && val !== null;
 	};
 
-	var bufferResizeEvent = function bufferResizeEvent() {
-		// buffer resize events so the browser doesn't fire a zillion of them
-		win.clearTimeout(resizeEventTimeoutID);
-		resizeEventTimeoutID = win.setTimeout(checkAllCorrals, 100);
+	var isArray = function isArray(val) {
+		return {}.toString.call(val) === '[object Array]';
 	};
 
-	var checkMediaQuery = function (mediaQuery) {
+	var resizeEventTimeoutID;
+
+	var throttleEvent = function throttleEvent(callback) {
+		// throttle events so the browser doesn't fire a zillion of them
+		win.clearTimeout(resizeEventTimeoutID);
+		resizeEventTimeoutID = win.setTimeout(callback, 100);
+	};
+
+	var tryMediaQuery = function (mediaQuery) {
 		if (typeof win.matchMedia === 'function') {
 			// look for native matchMedia first
 			return win.matchMedia(mediaQuery).matches;
@@ -24,97 +28,95 @@
 		return false;
 	};
 
+	// all instantiated corrals will be stored here so that checkAllCorrals()
+	// can iterate through them on each window resize event
+	var corrals = {};
+
 	var checkAllCorrals = function checkAllCorrals() {
 		// iterate through all corrals, checking the media query of each of
 		// their fences to determine whether we're inside those fences
 		for (var corralName in corrals) {
 			if (corrals.hasOwnProperty(corralName)) {
-				var corral = corrals[corralName];
-				var fences = ['min', 'max', 'both'];
-
-				// check each fence in this corral to see if we're in it
-				for (var i in fences) {
-					if (fences.hasOwnProperty(i)) {
-						var fence = fences[i];
-
-						if (checkMediaQuery(corral.queryString(fence))) {
-							// we've entered this fence
-							corral.trigger('enter-' + fence);
-						} else {
-							// we've exited this fence
-							corral.trigger('exit-' + fence);
-						}
-					}
-				}
+				corrals[corralName].check();
 			}
 		}
 	};
 
-	// all instantiated corrals will be stored here so that checkAllCorrals()
-	// can iterate through them on each window resize event
-	var corrals = {};
+	// valid action names
+	var actionNames = ['enter', 'exit'];
 
-	// valid event names
-	var eventNames = ['enter-min', 'enter-max', 'exit-min', 'exit-max', 'enter-both', 'exit-both'];
+	// check for same
+	var isActionName = function isActionName(actionName) {
+		return actionNames.indexOf(actionName) > -1;
+	};
+
+	// valid fence names
+	var fenceNames = ['min', 'max', 'both'];
+
+	// check for same
+	var isFenceName = function isFenceName(fenceName) {
+		return fenceNames.indexOf(fenceName) > -1;
+	};
 
 	// check for valid event names
 	var isEventName = function isEventName (eventName) {
-		return eventNames.indexOf(eventName) > -1;
+		try {
+			parseEventName(eventName);
+		} catch (e) {
+			return false;
+		}
+		return true;
 	};
 
-	// parse an event name into an event action, a callback action, an event
-	// fence, and a callback fence, e.g.:
+	// parse an event name into an action and a fence, e.g.:
 	//     {
-	//         event: {action: 'enter', fence: 'min'},
-	//         callback: {action: 'setup', fence: 'above'}
+	//         action: 'enter',
+	//         fence: 'min'
 	//     }
 	var parseEventName = function parseEventName (eventName) {
-		if (!isEventName(eventName)) {
-			return;
+		var splitEventName = eventName.split('-');
+
+		if (!isArray(splitEventName) || splitEventName.length < 2) {
+			throw new Error('parseEventName(): event name doesn\'t seem to have a hyphen');
 		}
 
-		eventName = eventName.split['-'];
-		var parsedEventInfo = {'event': {}, 'callback': {}};
-		parsedEventInfo.event.action = eventName[0];
-		parsedEventInfo.event.fence = eventName[1];
-		parsedEventInfo.callback.action = eventName[0] === 'enter' ? 'setup' : 'teardown';
-
-		switch (eventName[1]) {
-			case 'min':
-				// if we're inside the min fence, this means the
-				// constraint is 'this corral and above'
-				parsedEventInfo.callback.fence = 'above';
-				break;
-			case 'max':
-				// if we're inside the max fence, this means the
-				// constraint is 'this corral and below'
-				parsedEventInfo.callback.fence = 'below';
-				break;
-			case 'both':
-				// if we're inside both fences, this means the
-				// constraint is 'only this corral'
-				parsedEventInfo.callback.fence = 'only';
-				break;
+		if (!isActionName(splitEventName[0])) {
+			throw new Error('parseEventName(): action part of event name is invalid');
 		}
-
+		
+		if (!isFenceName(splitEventName[1])) {
+			throw new Error('parseEventName(): fence part of event name is invalid');
+		}
+		
+		var parsedEventInfo = { 'action': splitEventName[0], 'fence': splitEventName[1] };
 		return parsedEventInfo;
 	};
+
 
 	// check to see whether we've been passed a callback, an array of callbacks,
 	// or something invalid, clean it up, and pass it back as an array
 	var sanitiseCallbacks = function sanitiseCallbacks(callbacks) {
 		var sanitisedCallbacks = [];
 
-		if ({}.toString.call(callbacks) === '[object Array]') {
-			for (var i in callbacks) {
-				if (callbacks.hasOwnProperty[i] && typeof callbacks[i] == 'function') {
+		if (!isArray(callbacks)) {
+			if (typeof callbacks === 'function') {
+				callbacks = [callbacks];
+			} else {
+				throw new Error('sanitiseCallbacks(): callbacks argument was not a function or array');
+			}
+		}
+
+		for (var i in callbacks) {
+			if (callbacks.hasOwnProperty(i)) {
+				if (typeof callbacks[i] == 'function') {
 					sanitisedCallbacks.push(callbacks[i]);
+				} else {
+					throw new Error('sanitiseCallbacks(): got a bad callback -- wasn\'t a function');
 				}
 			}
-			return sanitisedCallbacks;
-		} else if (typeof callbacks === 'function') {
-			return [callbacks];
-		} // returns undefined if it's not an array
+		}
+
+		return sanitisedCallbacks;
 	};
 
 	// corral constructor
@@ -126,9 +128,9 @@
 			max: null
 		};
 		var _callbacks = {
-			'above': {'setup': [], 'teardown': [], 'isActive': false},
-			'below': {'setup': [], 'teardown': [], 'isActive': false},
-			'only': {'setup': [], 'teardown': [], 'isActive': false}
+			min: {enter: [], exit: [], active: false},
+			max: {enter: [], exit: [], active: false},
+			both: {enter: [], exit: [], active: false}
 		};
 		// add this corral to the list
 		var _this = corrals[newName] = this;
@@ -144,10 +146,23 @@
 
 		// were any callbacks passed? If so, add them to this corral
 		if (typeof newCallbacks === 'object') {
+			if (!newCallbacks.hasOwnProperty('min') &&
+				!newCallbacks.hasOwnProperty('max') &&
+				!newCallbacks.hasOwnProperty('both') && (
+					newCallbacks.hasOwnProperty('enter') ||
+					newCallbacks.hasOwnProperty('exit')
+				)
+			) {
+				// this doesn't have any specific sets of callbacks for each of
+				// the fences, but it has either entry or exit callbacks, so
+				// assume that it's a set of callbacks for 'both'
+				newCallbacks = {'both': newCallbacks};
+			}
+
 			for (var fence in newCallbacks) {
 				if (newCallbacks.hasOwnProperty(fence) && _callbacks.hasOwnProperty(fence)) {
-					_callbacks[fence].setup = sanitiseCallbacks(newCallbacks[fence].setup);
-					_callbacks[fence].teardown = sanitiseCallbacks(newCallbacks[fence].teardown);
+					_callbacks[fence].enter = sanitiseCallbacks(newCallbacks[fence].enter);
+					_callbacks[fence].exit = sanitiseCallbacks(newCallbacks[fence].exit);
 				}
 			}
 		}
@@ -181,11 +196,11 @@
 		};
 
 		// find out if a given constraint in this
-		this.isActive = function(callback) {
-			if (_callbacks.hasOwnProperty(callback)) {
-				return _callbacks[callback].isActive;
+		this.isActive = function isActive(fence) {
+			if (_callbacks.hasOwnProperty(fence)) {
+				return _callbacks[fence].active;
 			} else {
-				throw new Exception('Corral.isActive(): invalid callback name');
+				throw new Error('Corral.isActive(): invalid fence name');
 			}
 		};
 
@@ -193,19 +208,21 @@
 		this.on = function corralOn(eventName, callbacks) {
 			if (isEventName(eventName)) {
 				callbacks = sanitiseCallbacks(callbacks);
+
 				var eventInfo = parseEventName(eventName);
 
-				if (callbacks !== undefined) {
-					for (var i in callbacks) {
-						if (callbacks.hasOwnProperty(i)) {
-							_callbacks[eventInfo.callback.fence][eventInfo.callback.action].push(callbacks[i]);
+				for (var i in callbacks) {
+					if (callbacks.hasOwnProperty(i)) {
+						if (_this.isActive(eventInfo.fence) === (eventInfo.action === 'enter')) {
+							// run this callback if it applies to this corral at
+							// this time
+							callbacks[i]();
 						}
+						_callbacks[eventInfo.fence][eventInfo.action].push(callbacks[i]);
 					}
-				} else {
-					throw new Exception('Corral.on(): no callback(s) passed');
 				}
 			} else {
-				throw new Exception('Corral.on(): unknown eventName ' + eventName);
+				throw new Error('Corral.on(): unknown event name ' + eventName);
 			}
 		};
 
@@ -224,16 +241,16 @@
 							if (callbacks.hasOwnProperty(i)) {
 								// check for existence of callback in the already
 								// set up callbacks
-								var callbackIndex = _callbacks[eventInfo.callback.fence][eventInfo.callback.action].indexOf(callback);
+								var callbackIndex = _callbacks[eventInfo.fence][eventInfo.action].indexOf(callbacks[i]);
 								if (callbackIndex > -1) {
 									// only get rid of this function
-									delete _callbacks[eventInfo.callback.fence][eventInfo.callback.action][callbackIndex];
+									delete _callbacks[eventInfo.callback.fence][eventInfo.action][callbackIndex];
 								}
 							}
 						}
 					} else {
 						// something funky; we weren't passed any valid callbacks
-						throw new Exception('Corral.off(): no valid callbacks passed');
+						throw new Error('Corral.off(): no valid callbacks passed');
 					}
 				} else {
 					// if a callback wasn't specified, then delete all callbacks
@@ -245,82 +262,157 @@
 					}
 				}
 			} else {
-				throw new Exception('Corral.off(): unknown eventName ' + eventName);
+				throw new Error('Corral.off(): unknown eventName ' + eventName);
 			}
 		};
 
-		// event triggering
-		this.trigger = function corralTrigger(eventName) {
+		// event triggering -- takes an event name (consisting of an action,
+		// either 'enter' or 'exit'; and a fence, either 'min', 'max', or 'both'
+		// and separated by a hyphen) and an optional flag to force this event
+		// even if it's already active. The default behaviour is just to not
+		// bother running the callbacks on this event if it's already active.
+		this.trigger = function corralTrigger(eventName, force, triggerBoth) {
+			if (typeof triggerBoth !== 'boolean') {
+				triggerBoth = true;
+			}
+
 			// check to see if this is a valid event
 			if (isEventName(eventName)) {
 				var eventInfo = parseEventName(eventName);
+				var fenceCallbacks = _callbacks[eventInfo.fence];
 
-				if ((_this.isActive(eventInfo.callbacks.fence) && eventInfo.callbacks.action === 'setup') ||
-					(!_this.isActive(eventInfo.callbacks.fence) && eventInfo.callbacks.action === 'teardown')) {
+				if (_this.isActive(eventInfo.fence) === (eventInfo.action === 'enter') && !force) {
 					// don't bother firing if the event has already been fired
 					// -- either an exit event has been fired and the corral is
 					// already inactive, or an enter event has been fired and
-					// the corral is already active
+					// the fence is already active
 					return;
 				}
 
-				// set the active flag appropriately
-				_callbacks[eventInfo.callbacks.fence].isActive = (eventInfo.callbacks.fence === 'setup');
-
 				// setup or teardown the callbacks that pertain to this event
-				for (var i = 0; i < _callbacks[eventInfo.callbacks.fence][eventInfo.callbacks.action].length; i ++) {
-					_callbacks[eventInfo.callbacks.fence][eventInfo.callbacks.action][i]();
+				for (var i = 0; i < fenceCallbacks[eventInfo.action].length; i ++) {
+					fenceCallbacks[eventInfo.action][i]();
+				}
+
+				// set the active flag appropriately
+				fenceCallbacks.active = (eventInfo.action === 'enter');
+
+				if (eventInfo.fence === 'both') {
+					// set the active flag for both 'enter' and 'exit'
+					_callbacks.min.active = _callbacks.max.active = fenceCallbacks.active;
+				} else if (triggerBoth) {
+					// now process all 'both' callbacks -- that is, callbacks
+					// that apply only when we're in both fences of this corral
+					var complement = (eventInfo.fence === 'min' ? 'max' : 'min');
+
+					if (_this.isActive(complement)) {
+						// only run the 'both' callbacks if the other fence is active too
+						if (_this.isActive('both') !== (eventInfo.action === 'enter') && !force) {
+							// as with above, don't run the 'both' callbacks if they're
+							// already active, unless the force flag has been set
+							_this.trigger(eventInfo.action + '-both');
+						}
+					}
 				}
 			} else {
-				throw new Exception('Corral.trigger(): unknown eventName ' + eventName);
+				throw new Error('Corral.trigger(): unknown eventName ' + eventName + ', or else eventName has \'both\' in it');
 			}
 		};
 
-		// media query generation
-		this.queryString = function queryString(fence) {
-			switch (fence) {
-				case 'min':
-				case 'max':
-					return '(' + fence + '-' + _this.axis() + ':' + _this[fence]() + ')';
-				case 'both':
-					return _this.queryString('min') + ' and ' + _this.queryString('max');
-			}
-		};
+		// after construction, check this corral to see if it's active
+		this.check();
 	};
 
+	// cleanly remove a corral so it doesn't fire anymore
+	Corral.prototype.remove = function deleteThisCorral() {
+		Corral.remove(this);
+	};
+
+	// check to see if we're inside one or both of this corral's fences
+	Corral.prototype.check = function checkThisCorral() {
+		var fences = ['min', 'max'],
+			fence;
+
+		for (var i in fences) {
+			if (fences.hasOwnProperty(i)) {
+				fence = fences[i];
+
+				if (tryMediaQuery(this.queryString(fence))) {
+					if (!this.isActive(fence)) {
+						this.trigger('enter-' + fence);
+					}
+				} else {
+					if (this.isActive(fence)) {
+						this.trigger('exit-' + fence);
+					}
+				}
+			}
+		}
+	};
+
+	// generate a media query for use in the above check
+	Corral.prototype.queryString = function queryString(fence) {
+		if (fence === 'min' || fence === 'max') {
+			var fenceValue = this[fence]();
+			if (!exists(fenceValue)) {
+				// if this fence happens to be missing, assume 0 for the
+				// bottom end and infinity for the top end. Of course, these
+				// don't work well for media queries, so the bottom end is
+				// 1px and the top end is 20000px. This allows you to create
+				// open-ended corrals (e.g., no minimum fence)
+				if (fence === 'min') {
+					fenceValue = '1px';
+				} else {
+					fenceValue = '20000px';
+				}
+			}
+			return '(' + fence + '-' + this.axis() + ':' + fenceValue + ')';
+		} else {
+			throw new Error('Corral.queryString(): fence should be min or max');
+		}
+	};
+
+	// bind, unbind, or trigger an event on a specific corral -- used in the
+	// functions below
 	Corral._event = function _event (eventType, corral, eventName, callback) {
-		if (bindType === 'on' || bindType === 'off' || bindType === 'trigger') {
-			if (bindType === 'trigger') {
+		if (eventType === 'on' || eventType === 'off' || eventType === 'trigger') {
+			if (eventType === 'trigger') {
 				// the trigger event is handled by this method, but it shouldn't
 				// have any callback
 				callback = undefined;
 			}
 			if (corral instanceof Corral) {
 				// passed corral is a Corral object; call it directly
-				return corral[bindType](eventName, callback);
+				return corral[eventType](eventName, callback);
 			} else if (corrals.hasOwnProperty(corral)) {
 				// passed corral is a corral name; call it from the hash of
 				// instantiated corrals
-				return corrals[corral][bindType](eventName, callback);
+				return corrals[corral][eventType](eventName, callback);
 			}
 		} else {
-			throw new Exception('Corral._bindOrUnbind(): bindType ' + bindType + 'is invalid');
+			throw new Error('Corral._event(): eventType ' + eventType + 'is invalid');
 		}
 	};
 
+	// bind a function to an event on a specific corral
 	Corral.on = function onCorral(corral, eventName, callback) {
 		return Corral._event('on', corral, eventName, callback);
 	};
 
+	// unbind a function to an event on a specific corral
 	Corral.off = function offCorral(corral, eventName, callback) {
 		return Corral._event('off', corral, eventName, callback);
 	};
 
+	// trigger an event on a specific corral
 	Corral.trigger = function triggerCorral(corral, eventName) {
 		return Corral._event('trigger', corral, eventName);
 	};
 
+	// cleanly delete a corral by reference or by name
 	Corral.remove = function deleteCorral(corral) {
+		var corralName;
+
 		if (corral instanceof Corral) {
 			corralName = corral.name;
 		} else {
@@ -334,16 +426,56 @@
 		return false;
 	};
 
-	Corral.prototype.remove = function deleteThisCorral() {
-		Corral.remove(this);
+	Corral.get = function getCorral(corralName) {
+		if (corrals.hasOwnProperty(corralName)) {
+			return corrals[corralName];
+		}
+	}
+
+	// get a hash of all breakpoints, their axes, and min/max values. Returns an
+	// object in this format:
+	//     {
+	//          mobile: { axis: 'width', max: '20em' },
+	//         	tablet: { axis: 'width', min: '20.0001em', max: '59.9999em' },
+	//          desktop: { axis: 'width', min: '60em' }
+	//          // et cetera for all the breakpoints you've defined
+	//     }
+	Corral.breakpoints = function getAllBreakpoints() {
+		var breakpoints = {},
+			corralName,
+			breakpoint,
+			corral;
+
+		for (corralName in corrals) {
+			if (corrals.hasOwnProperty(corralName)) {
+				corral = corrals[corralName];
+				breakpoint = {};
+				breakpoint.axis = corral.axis();
+
+				if (exists(corrals[breakpoints].min())) {
+					breakpoint.min = corrals[breakpoints].min();
+				}
+				if (exists(corrals[breakpoints].max())) {
+					breakpoint.max = corrals[breakpoints].max();
+				}
+
+				breakpoints[corralName] = breakpoint;
+			}
+		}
+
+		return breakpoints;
+	};
+
+	var checkAllCorralsThrottled = function checkAllCorralsThrottled () {
+		throttleEvent(checkAllCorrals);
 	};
 
 	if (win.addEventListener) {
-		win.addEventListener('resize', bufferResizeEvent, false);
+		win.addEventListener('resize', checkAllCorralsThrottled, false);
 	} else if (win.attachEvent) {
-		win.attachEvent('onresize', bufferResizeEvent);
+		win.attachEvent('onresize', checkAllCorralsThrottled);
 	} else {
-		win.onresize = bufferResizeEvent;
+		win.onresize = checkAllCorralsThrottled;
 	}
 
 }(window));
